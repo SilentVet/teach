429164c60d81a2baa89ff8c943f1bed23b711e06
vmarchukov <v.marchukov@voskhod.ru>
2021-07-06 11:05:25 +0400

    New files

---
+++ src/Coordinats.java
@@ -0,0 +1,13 @@
+import static java.lang.Math.sqrt;
+public class Coordinats {
+
+    public static void main(String[] args) {
+        Point a = new Point(80, 8);
+
+        System.out.println("Координаты равны" + " " + a.destination());
+
+    }
+}
+
+
+
---
+++ src/Point.java
@@ -0,0 +1,14 @@
+import static java.lang.Math.sqrt;
+
+public class Point {
+    public double a;
+    public double b;
+
+    public  Point(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+    public double destination() {
+        return sqrt((this.a - this.b) + (this.a - this.b));
+    }
+    }
